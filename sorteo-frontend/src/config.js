56b7108acf5f332cce78a187093cb794c65280ab
Luciano Zurlo <luciano@aiMac.local>
2025-02-03 15:57:32 -0300

    all

---
+++ sorteo-frontend/src/config.js
@@ -0,0 +1,7 @@
+// src / config.js;
+export const API_BASE_URL =
+  process.env.REACT_APP_API_BASE_URL || 'http://127.0.0.1:8000';
+
+// export const API_BASE_URL =
+//   process.env.REACT_APP_API_BASE_URL ||
+//   'https://sorteos-ci-production.up.railway.app/';
---
+++ sorteo_project/asgi.py
@@ -0,0 +1,8 @@
+# sorteo_project/asgi.py
+
+import os
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sorteo_project.settings")
+
+application = get_asgi_application()
